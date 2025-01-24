25cc6ea28185a3dc489a92a981d6357fd29f1172
RedondoDEV <javierRedondoDEV@gmail.com>
2025-01-24 18:41:58 +0100

    mail system working

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
 app.config['MAIL_USERNAME'] = os.getenv("MAIL")
 app.config['MAIL_PASSWORD'] = os.getenv("PASS")
 
@@ -48,10 +49,11 @@ def save_comment():
     db.session.commit()
 
     msg = Message(
-        subject="op.jj form",
+        subject="op.jj form: " + name,
         sender=email,
         recipients=[os.getenv("MAIL")],
-        body=comment
+        body=email + "\n\n" + comment,
+        reply_to=email
     )
     mail.send(msg)
     return redirect('/about_me')
