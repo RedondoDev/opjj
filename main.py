2ae173d686a3328b28f389aa67e83410aa4ba008
RedondoDEV <javierRedondoDEV@gmail.com>
2025-01-24 17:38:22 +0100

    env done

--- main.py
+++ main.py
@@ -2,23 +2,36 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from flask import request, redirect
+from flask_mail import Mail, Message
+from dotenv import load_dotenv
+import os
+
+load_dotenv("variables.env")
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.getenv("MAIL")
+app.config['MAIL_PASSWORD'] = os.getenv("PASS")
+
 db = SQLAlchemy(app)
+mail = Mail(app)
 
 
 class Commentary(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.DateTime, default=datetime.now)
+    email = db.Column(db.String(50))
     name = db.Column(db.String(50))
     comment = db.Column(db.String(500))
 
     def __repr__(self):
-        return f"<Commentary {self.id} - {self.date} - {self.name} - {self.comment}>"
+        return f"<Commentary {self.id} - {self.date} - {self.email} - {self.name} - {self.comment}>"
 
 
 with app.app_context():
@@ -28,10 +41,19 @@ with app.app_context():
 @app.route('/about_me/comment', methods=['POST'])
 def save_comment():
     name = request.form.get('name')
+    email = request.form.get('email')
     comment = request.form.get('comment')
-    new_comment = Commentary(name=name, comment=comment, date=datetime.now())
+    new_comment = Commentary(name=name, email=email, comment=comment, date=datetime.now())
     db.session.add(new_comment)
     db.session.commit()
+
+    msg = Message(
+        subject="op.jj form",
+        sender=email,
+        recipients=[os.getenv("MAIL")],
+        body=comment
+    )
+    mail.send(msg)
     return redirect('/about_me')
 
 
