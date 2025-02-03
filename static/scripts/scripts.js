b79b4bf880f5791e969ef914a7cc1dbe7bf5c14b
RedondoDEV <javierRedondoDEV@gmail.com>
2025-02-03 11:18:18 +0100

    stats problems solved

--- static/scripts/scripts.js
+++ static/scripts/scripts.js
@@ -40,8 +40,6 @@ async function getRiotPuuids() {
             fetchPuuid(region1, gameName1, tagLine1),
             fetchPuuid(region2, gameName2, tagLine2)
         ]);
-        console.log(`${summoner1.region} ${summoner1.gameName} ${summoner1.tagLine} ${summoner1.puuid}`);
-        console.log(`${summoner2.region} ${summoner2.gameName} ${summoner2.tagLine} ${summoner2.puuid}`);
         await getHiddenIds(summoner1, summoner2)
     } catch (error) {
         alert("Request failed. Please check your inputs and try again.");
@@ -79,21 +77,28 @@ async function fetchHiddenIds(summoner) {
 }
 
 
-async function fetchSoloQStats(summoner) {
+async function fetchSoloQFlexStats(summoner) {
     const url = `/api/fetch_soloq_stats?region=${summoner.region}&summonerId=${summoner.id}`;
     try {
         const response = await fetch(url);
         if (response.ok) {
             let rankedStats = await response.json();
             if (rankedStats.length === 0) {
-                await addDefaultStats(summoner);
-                console.log(summoner.rankedStats[0]);
+                await addDefaultStats(summoner, 0, "RANKED_SOLO_5x5");
+                await addDefaultStats(summoner, 1, "RANKED_FLEX_SR");
             } else {
-                rankedStats.forEach(stat => {
-                    summoner.rankedStats.push(stat);
+                rankedStats.sort((a, b) => {
+                    if (a.queueType === "RANKED_SOLO_5x5") return -1;
+                    if (b.queueType === "RANKED_SOLO_5x5") return 1;
+                    if (a.queueType === "RANKED_FLEX_SR") return -1;
+                    if (b.queueType === "RANKED_FLEX_SR") return 1;
+                    return 0;
                 });
+                summoner.rankedStats = rankedStats;
+                if (!summoner.rankedStats[1] || summoner.rankedStats[1].queueType !== "RANKED_FLEX_SR") {
+                    await addDefaultStats(summoner, 1, "RANKED_FLEX_SR");
+                }
             }
-            console.log(`Found ranked stats:`, summoner.rankedStats[0]);
         } else {
             console.log(`No ranked stats found, response code: ${response.status}`);
         }
@@ -103,45 +108,33 @@ async function fetchSoloQStats(summoner) {
 }
 
 
-function showSummonners(summoners) {
-    let name1 = document.getElementById("oneName");
-    name1.innerText = `${summoners[0].gameName}`;
-    let tag1 = document.getElementById("oneTag");
-    tag1.innerText = `#${summoners[0].tagLine}`;
-    let level1 = document.getElementById("oneLevel");
-    level1.innerText = `Level ${summoners[0].lvl}`;
-    let region1 = document.getElementById("oneRegion");
-    region1.innerText = summoners[0].region.toUpperCase();
-    let image1 = document.getElementById("oneImage");
-    image1.src = `https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/profile-icons/${summoners[0].profileIconId}.jpg`;
+function showSummoners(summoners) {
+    const elements = [
+        {name: "one", summoner: summoners[0]},
+        {name: "two", summoner: summoners[1]}
+    ];
 
-    let name2 = document.getElementById("twoName");
-    name2.innerText = `${summoners[1].gameName}`;
-    let tag2 = document.getElementById("twoTag");
-    tag2.innerText = `#${summoners[1].tagLine}`;
-    let level2 = document.getElementById("twoLevel");
-    level2.innerText = `Level ${summoners[1].lvl}`;
-    let region2 = document.getElementById("twoRegion");
-    region2.innerText = summoners[1].region.toUpperCase();
-    let image2 = document.getElementById("twoImage");
-    image2.src = `https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/profile-icons/${summoners[1].profileIconId}.jpg`;
+    elements.forEach(({name, summoner}) => {
+        document.getElementById(`${name}Name`).innerText = summoner.gameName;
+        document.getElementById(`${name}Tag`).innerText = `#${summoner.tagLine}`;
+        document.getElementById(`${name}Level`).innerText = `Level ${summoner.lvl}`;
+        document.getElementById(`${name}Region`).innerText = summoner.region.toUpperCase();
+        document.getElementById(`${name}Image`).src = `https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/profile-icons/${summoner.profileIconId}.jpg`;
+    });
 }
 
 
 function showSoloQStats(summoners) {
-    let name1 = document.getElementById("oneNameRank");
-    name1.innerText = `${summoners[0].gameName} #${summoners[0].tagLine}`;
-    let lps1 = document.getElementById("span1Lps");
-    lps1.innerText = `${summoners[0].rankedStats[0].leaguePoints}`;
-    let tier1 = document.getElementById("oneRank");
-    tier1.innerText = `${summoners[0].rankedStats[0].tier} ${summoners[0].rankedStats[0].rank}`;
-    let wins1 = document.getElementById("oneWins");
-    wins1.innerText = `W: ${summoners[0].rankedStats[0].wins}`;
-    let losses1 = document.getElementById("oneLosses");
-    losses1.innerText = `L: ${summoners[0].rankedStats[0].losses}`;
-    let games1 = document.getElementById("oneGames");
-    games1.innerText = `G: ${(summoners[0].rankedStats[0].wins + summoners[0].rankedStats[0].losses)}`
-    let winRate = (summoners[0].rankedStats[0].wins / (summoners[0].rankedStats[0].wins + summoners[0].rankedStats[0].losses)) * 100;
+    document.getElementById("oneNameRank").innerText = `${summoners[0].gameName} #${summoners[0].tagLine}`;
+    document.getElementById("span1Lps").innerText = `${summoners[0].rankedStats[0].leaguePoints}`;
+    document.getElementById("oneRank").innerText = `${summoners[0].rankedStats[0].tier} ${summoners[0].rankedStats[0].rank}`;
+    document.getElementById("oneWins").innerText = `W: ${summoners[0].rankedStats[0].wins}`;
+    document.getElementById("oneLosses").innerText = `L: ${summoners[0].rankedStats[0].losses}`;
+    document.getElementById("oneGames").innerText = `G: ${summoners[0].rankedStats[0].wins + summoners[0].rankedStats[0].losses}`;
+    let winRate = 0;
+    if (summoners[0].rankedStats[0].wins + summoners[0].rankedStats[0].losses > 0) {
+        winRate = (summoners[0].rankedStats[0].wins / (summoners[0].rankedStats[0].wins + summoners[0].rankedStats[0].losses)) * 100;
+    }
     let wr1 = document.getElementById("span1WR");
     wr1.innerText = `${winRate.toFixed(2)}%`;
     let floatWinRate1 = parseFloat(wr1.innerText);
@@ -149,28 +142,22 @@ function showSoloQStats(summoners) {
     let image1 = document.getElementById("oneImageTier");
     image1.src = `static/resources/ranks/${summoners[0].rankedStats[0].tier}.png`;
 
-    let name2 = document.getElementById("twoNameRank");
-    name2.innerText = `${summoners[1].gameName} #${summoners[1].tagLine}`;
-    let lps2 = document.getElementById("span2Lps");
-    lps2.innerText = `${summoners[1].rankedStats[0].leaguePoints}`;
-    let tier2 = document.getElementById("twoRank");
-    tier2.innerText = `${summoners[1].rankedStats[0].tier} ${summoners[1].rankedStats[0].rank}`;
-    let wins2 = document.getElementById("twoWins");
-    wins2.innerText = `W: ${summoners[1].rankedStats[0].wins}`;
-    let losses2 = document.getElementById("twoLosses");
-    losses2.innerText = `L: ${summoners[1].rankedStats[0].losses}`;
-    let games2 = document.getElementById("twoGames");
-    games2.innerText = `G: ${(summoners[1].rankedStats[0].wins + summoners[1].rankedStats[0].losses)}`
-    let winRate2 = (summoners[1].rankedStats[0].wins / (summoners[1].rankedStats[0].wins + summoners[1].rankedStats[0].losses)) * 100;
+    document.getElementById("twoNameRank").innerText = `${summoners[1].gameName} #${summoners[1].tagLine}`;
+    document.getElementById("span2Lps").innerText = `${summoners[1].rankedStats[0].leaguePoints}`;
+    document.getElementById("twoRank").innerText = `${summoners[1].rankedStats[0].tier} ${summoners[1].rankedStats[0].rank}`;
+    document.getElementById("twoWins").innerText = `W: ${summoners[1].rankedStats[0].wins}`;
+    document.getElementById("twoLosses").innerText = `L: ${summoners[1].rankedStats[0].losses}`;
+    document.getElementById("twoGames").innerText = `G: ${summoners[1].rankedStats[0].wins + summoners[1].rankedStats[0].losses}`;
+    let winRate2 = 0;
+    if (summoners[1].rankedStats[0].wins + summoners[1].rankedStats[0].losses > 0) {
+        winRate2 = (summoners[1].rankedStats[0].wins / (summoners[1].rankedStats[0].wins + summoners[1].rankedStats[0].losses)) * 100;
+    }
     let wr2 = document.getElementById("span2WR");
     wr2.innerText = `${winRate2.toFixed(2)}%`;
     let floatWinRate2 = parseFloat(wr2.innerText);
     stylesIfNan(wr2, floatWinRate2);
     let image2 = document.getElementById("twoImageTier");
     image2.src = `static/resources/ranks/${summoners[1].rankedStats[0].tier}.png`;
-
-    console.log(summoners[0].rankedStats[0]);
-    console.log(summoners[1].rankedStats[0]);
 }
 
 
@@ -186,8 +173,12 @@ function showFlexQStats(summoners) {
     let losses1f = document.getElementById("oneLossesFlex");
     losses1f.innerText = `L: ${summoners[0].rankedStats[1].losses}`;
     let games1f = document.getElementById("oneGamesFlex");
-    games1f.innerText = `G: ${(summoners[0].rankedStats[1].wins + summoners[0].rankedStats[1].losses)}`
-    let winRatef = (summoners[0].rankedStats[1].wins / (summoners[0].rankedStats[1].wins + summoners[0].rankedStats[1].losses)) * 100;
+    games1f.innerText = `G: ${(summoners[0].rankedStats[1].wins + summoners[0].rankedStats[1].losses)}`;
+
+    let winRatef = 0;
+    if (summoners[0].rankedStats[1].wins + summoners[0].rankedStats[1].losses > 0) {
+        winRatef = (summoners[0].rankedStats[1].wins / (summoners[0].rankedStats[1].wins + summoners[0].rankedStats[1].losses)) * 100;
+    }
     let wr1f = document.getElementById("span1WRFlex");
     wr1f.innerText = `${winRatef.toFixed(2)}%`;
     let floatWinRate1f = parseFloat(wr1f.innerText);
@@ -203,20 +194,21 @@ function showFlexQStats(summoners) {
     tier2f.innerText = `${summoners[1].rankedStats[1].tier} ${summoners[1].rankedStats[1].rank}`;
     let wins2f = document.getElementById("twoWinsFlex");
     wins2f.innerText = `W: ${summoners[1].rankedStats[1].wins}`;
-    let lossesf2 = document.getElementById("twoLossesFlex");
-    lossesf2.innerText = `L: ${summoners[1].rankedStats[1].losses}`;
-    let gamesf2 = document.getElementById("twoGamesFlex");
-    gamesf2.innerText = `G: ${(summoners[1].rankedStats[1].wins + summoners[1].rankedStats[1].losses)}`
-    let winRatef2 = (summoners[1].rankedStats[1].wins / (summoners[1].rankedStats[1].wins + summoners[1].rankedStats[1].losses)) * 100;
-    let wrf2 = document.getElementById("span2WRFlex");
-    wr1f.innerText = `${winRatef2.toFixed(2)}%`;
-    let floatWinRate2f = parseFloat(wrf2.innerText);
-    stylesIfNan(wrf2, floatWinRate2f);
+    let losses2f = document.getElementById("twoLossesFlex");
+    losses2f.innerText = `L: ${summoners[1].rankedStats[1].losses}`;
+    let games2f = document.getElementById("twoGamesFlex");
+    games2f.innerText = `G: ${(summoners[1].rankedStats[1].wins + summoners[1].rankedStats[1].losses)}`;
+
+    let winRatef2 = 0;
+    if (summoners[1].rankedStats[1].wins + summoners[1].rankedStats[1].losses > 0) {
+        winRatef2 = (summoners[1].rankedStats[1].wins / (summoners[1].rankedStats[1].wins + summoners[1].rankedStats[1].losses)) * 100;
+    }
+    let wr2f = document.getElementById("span2WRFlex");
+    wr2f.innerText = `${winRatef2.toFixed(2)}%`;
+    let floatWinRate2f = parseFloat(wr2f.innerText);
+    stylesIfNan(wr2f, floatWinRate2f);
     let image2f = document.getElementById("twoImageTierFlex");
     image2f.src = `static/resources/ranks/${summoners[1].rankedStats[1].tier}.png`;
-
-    console.log(summoners[0].rankedStats[1]);
-    console.log(summoners[1].rankedStats[1]);
 }
 
 
@@ -240,12 +232,9 @@ async function getHiddenIds(summoner1, summoner2) {
     let summoners = [summoner1, summoner2];
     try {
         await Promise.all(summoners.map(summoner => fetchHiddenIds(summoner)));
-        await Promise.all(summoners.map(summoner => fetchSoloQStats(summoner)));
+        await Promise.all(summoners.map(summoner => fetchSoloQFlexStats(summoner)));
 
-        console.log(summoner1.rankedStats[0]);
-        console.log(summoner2.rankedStats[0]);
-
-        showSummonners(summoners);
+        showSummoners(summoners);
         showSoloQStats(summoners);
         showFlexQStats(summoners);
     } catch (error) {
@@ -259,8 +248,8 @@ async function getHiddenIds(summoner1, summoner2) {
 }
 
 
-async function addDefaultStats(summoner) {
-    summoner.rankedStats[0] = {
+async function addDefaultStats(summoner, index, queueType) {
+    summoner.rankedStats[index] = {
         tier: "UNRANKED",
         rank: "",
         leaguePoints: "0",
@@ -269,17 +258,6 @@ async function addDefaultStats(summoner) {
         inactive: true,
         freshBlood: "",
         hotStreak: "",
-        queueType: "RANKED_SOLO_5x5"
-    };
-    summoner.rankedStats[1] = {
-        tier: "UNRANKED",
-        rank: "",
-        leaguePoints: "0",
-        wins: 0,
-        losses: 0,
-        inactive: true,
-        freshBlood: "",
-        hotStreak: "",
-        queueType: "RANKED_FLEX_5x5"
+        queueType: queueType
     };
 }
